e0cea513ded1c8c6f5d729a2d323bd5b3431e43f
Hardikag17 <hardikag17@gmail.com>
2022-08-10 20:38:45 +0530

    gunjs dev env

--- frontend/src/components/Body/userData.jsx
+++ frontend/src/components/Body/userData.jsx
@@ -62,9 +62,9 @@ export default function UserDate() {
 
   return (
     <div className=' w-full flex flex-col'>
-      <div className=' flex flex-row border-solid border-grey border-2 w-full'>
-        <div className=' text-left flex items-center w-full font-bold p-1 text-mobile_large px-2'>
-          $Tezos: 523
+      <div className=' h-72 flex flex-row border-solid border-grey border-2 w-full'>
+        <div className=' text-left flex items-center w-full font-bold p-1 text-web_large px-2'>
+          Current Balance: 500 Tez
         </div>
         <div className=' flex flex-row justify-end'>
           <button class='text-gray rounded-lg border-2 m-1 border-solid border-grey focus:outline-none w-24 h-12 '>
@@ -78,18 +78,26 @@ export default function UserDate() {
           </button>
         </div>
       </div>
-      <div className='absolute flex flex-col justify-end right-0 bottom-0 w-4/6'>
-        <div className='text-grey py-2 lg:px-2 text-small text-center'>
-          {/* {state.messages.map((message) => (
-            <div key={message.createdAt}>
-              <h2>{message.message}</h2>
-              <h3>From: {message.name}</h3>
-              <p>Date: {message.createdAt}</p>
-            </div>
-          ))} */}
+      <div className='flex flex-col mx-5'>
+        <div className='text-grey py-2 lg:px-2 text-small text-center w-full'>
+          <div className='h-96 flex flex-wrap overflow-y-scroll snap snap-y snap-mandatory hide-scroll-bar justify-center w-full items-center mx-auto'>
+            {state.messages.map((message) => (
+              <div
+                className='flex-shrink-0 flex-col snap-always snap-center w-full mx-auto bg-white my-2 rounded-lg text-black'
+                key={message.createdAt}>
+                <div className=' flex flex-row space-x-4 '>
+                  <h3>From: {message.name}</h3>
+                  <p>Date: {message.createdAt}</p>
+                </div>
+                <div>
+                  <h2 className=' text-web_large'>{message.message}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
           <i>Your Chats are end to end encrypted</i>
         </div>
-        <div class='bg-black rounded-full w-full flex items-center border-2 border-solid border-grey shadow-xl'>
+        <div class='absolute bg-black rounded-full w-4/6 right-0 bottom-0 flex items-center border-2 border-solid border-grey shadow-xl'>
           <input
             className='rounded-l text-small bg-black w-full px-4 text-gray leading-tight focus:outline-none'
             id='search'
